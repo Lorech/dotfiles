f8529a92ab6f424b46dbdc73cb2e3f16b209b0d1
Lorech <laurisdedumets@gmail.com>
2024-09-26 22:46:20 +0300

    feat: Add custom configuration for Neotree
    - Updates shortcuts for opening/closing Neotree
    - Bumps Neotree width
    - Repositions Neotree
    - Adds custom keymaps for navigation and file management

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -9,13 +9,43 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>wr', ':Neotree filesystem reveal<CR>', desc = '[W]orkspace [R]eveal', silent = true },
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+      },
       window = {
+        width = 60,
         mappings = {
-          ['\\'] = 'close_window',
+          ['<leader>wr'] = { 'close_window', desc = '[W]orkspace un[R]eveal' },
+        },
+      },
+    },
+    window = {
+      position = 'right',
+      mappings = {
+        ['<space>'] = 'none',
+        ['l'] = 'open',
+        ['h'] = 'close_node',
+        ['Y'] = {
+          function(state)
+            local node = state.tree:get_node()
+            local absPath = node:get_id()
+            local cwd = vim.fn.getcwd()
+            local relPath = string.gsub(absPath, cwd, '')
+            vim.fn.setreg('+', relPath, 'c')
+          end,
+          desc = '[Y]ank relative path to clipboard',
+        },
+        ['YY'] = {
+          function(state)
+            local node = state.tree:get_node()
+            local absPath = node:get_id()
+            vim.fn.setreg('+', absPath, 'c')
+          end,
+          desc = '[Y]ank absolute path to clipboard',
         },
       },
     },
