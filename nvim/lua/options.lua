1837ff1a787a02a7152d30e3ca36ea8602c6445c
Lorech <laurisdedumets@gmail.com>
2024-09-30 21:59:27 +0300

    fix: Allow selecting whitespace during block actions

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -52,3 +52,6 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor
 vim.opt.scrolloff = 10
+
+-- Allow selecting whitespace in block editing mode
+vim.opt.virtualedit = 'block'
