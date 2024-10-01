8ec63de0165a163946221e21d5f7242a0e27c8cf
Lorech <laurisdedumets@gmail.com>
2024-10-01 20:45:51 +0300

    feat: Configure visual appearance
    - Hide tab bar
    - Set custom font
    - Set custom theme

--- wezterm/appearance.lua
+++ wezterm/appearance.lua
@@ -3,6 +3,15 @@ local wezterm = require 'wezterm'
 local appearance = {}
 
 function appearance.apply_to_config(config)
+  -- Window
+  config.enable_tab_bar = false
+
+  -- Text
+  config.font = wezterm.font 'CaskaydiaMono Nerd Font'
+  config.font_size = 16
+
+  -- Colors
+  config.color_scheme = 'rose-pine-moon'
 end
 
 return appearance
