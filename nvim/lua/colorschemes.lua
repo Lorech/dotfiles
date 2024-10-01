4c8d77f00c3bf9b848db757b700388611adf62bd
Lorech <laurisdedumets@gmail.com>
2024-10-01 16:38:11 +0300

    refactor: Simplify colorscheme management

--- nvim/lua/colorschemes.lua
+++ nvim/lua/colorschemes.lua
@@ -1,8 +1,29 @@
--- Tokyonight theme
-return {
+-- Colorscheme plugins to keep installed using lazy.nvim
+local ensure_installed = {
   'folke/tokyonight.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins
-  init = function()
-    vim.cmd.colorscheme 'tokyonight-night'
-  end,
 }
+
+-- The current colorscheme to use
+--
+-- Setting plugin will ensure that this specific plugin gets priority loading
+-- within lazy.nvim, and will apply the provided colorscheme within Neovim
+-- when this plugin initializes, setting the custom colorscheme as soon as possible
+local current = {
+  plugin = 'folke/tokyonight.nvim',
+  colorscheme = 'tokyonight-night',
+}
+
+-- Generate a configuration to load all the defined colorschemes with lazy.nvim
+local colorschemes = {}
+for _, plugin in pairs(ensure_installed) do
+  local colorscheme = { plugin, lazy = true }
+  if plugin == current.plugin then
+    colorscheme['lazy'] = false
+    colorscheme['priority'] = 1000
+    colorscheme['init'] = function()
+      vim.cmd.colorscheme(current.colorscheme)
+    end
+  end
+  table.insert(colorschemes, colorscheme)
+end
+return colorschemes
