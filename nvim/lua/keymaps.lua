2cdb7fb18eee4c7043c7f51db5e48f20321fe7f8
Lorech <laurisdedumets@gmail.com>
2024-10-01 19:09:45 +0300

    chore: Remove built-in location list keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -4,9 +4,6 @@
 -- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'open diagnostic Quickfix list' })
-
 -- Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!"<CR>')
