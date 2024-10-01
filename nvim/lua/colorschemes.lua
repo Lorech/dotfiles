e5d36e34acf4c17a878e8cb192e5bf3e3678baac
Lorech <laurisdedumets@gmail.com>
2024-10-01 17:17:27 +0300

    refactor: Simplify colorscheme management further
    - Specifies all the colorschemes as tables
    - Supports renaming the plugin for lazy.nvim
    - Allows toggling two boolean values instead of duplicating plugin name
    when changing active theme

--- nvim/lua/colorschemes.lua
+++ nvim/lua/colorschemes.lua
@@ -1,28 +1,41 @@
 -- Colorscheme plugins to keep installed using lazy.nvim
-local ensure_installed = {
-  'folke/tokyonight.nvim',
-  'rose-pine/neovim',
-}
-
--- The current colorscheme to use
 --
--- Setting plugin will ensure that this specific plugin gets priority loading
--- within lazy.nvim, and will apply the provided colorscheme within Neovim
--- when this plugin initializes, setting the custom colorscheme as soon as possible
-local current = {
-  plugin = 'rose-pine/neovim',
-  colorscheme = 'rose-pine-moon',
+-- A name property can be provided to change how the plugin is called within lazy.nvim
+--
+-- A colorscheme property should be provided to determine which colorscheme will be loaded
+-- if that specific theme has the active property set to true
+--
+-- Active should be set true to the theme you wish to use to ensure it loads ASAP, and to
+-- change which colorscheme will get initialized as the active one in Neovim. If active is
+-- not set, it is assumed that the theme is not active, and therefore will not be loaded
+--
+-- See `:Telescope colorscheme` to preview the themes
+local ensure_installed = {
+  {
+    'folke/tokyonight.nvim',
+    colorscheme = 'tokyonight-night',
+    active = false,
+  },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    colorscheme = 'rose-pine-moon',
+    active = true,
+  },
 }
 
 -- Generate a configuration to load all the defined colorschemes with lazy.nvim
 local colorschemes = {}
 for _, plugin in pairs(ensure_installed) do
-  local colorscheme = { plugin, lazy = false }
-  if plugin == current.plugin then
+  local colorscheme = { plugin[1], lazy = false }
+  if plugin.name then
+    colorscheme['name'] = plugin.name
+  end
+  if plugin.active then
     colorscheme['lazy'] = true
     colorscheme['priority'] = 1000
     colorscheme['init'] = function()
-      vim.cmd.colorscheme(current.colorscheme)
+      vim.cmd.colorscheme(plugin.colorscheme)
     end
   end
   table.insert(colorschemes, colorscheme)
