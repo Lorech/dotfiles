3b76587b5a8e40c0daaeb88240e4d0e5d4976584
Lorech <laurisdedumets@gmail.com>
2024-10-01 20:46:03 +0300

    feat: Add custom split shortcuts

--- wezterm/keymaps.lua
+++ wezterm/keymaps.lua
@@ -3,6 +3,29 @@ local wezterm = require 'wezterm'
 local keymaps = {}
 
 function keymaps.apply_to_config(config)
+  config.leader = { key = 'a', mods = 'CTRL' }
+  config.keys = {
+    -- Splits
+    {
+      key = '|',
+      mods = 'LEADER|SHIFT',
+      action = wezterm.action {
+        SplitHorizontal = {
+          domain = 'CurrentPaneDomain',
+        },
+      },
+    },
+    {
+      key = '_',
+      mods = 'LEADER|SHIFT',
+      action = wezterm.action {
+        SplitVertical = {
+          domain = 'CurrentPaneDomain',
+        },
+      },
+    },
+    -- Navigation
+  }
 end
 
 return keymaps
