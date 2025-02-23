cbe2cdea49c27229d36d533aeaca3e3a9b4690f1
Lorech <laurisdedumets@gmail.com>
2025-02-23 15:02:04 +0200

    chore: Remove Hop

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,7 +29,6 @@ require('lazy').setup({
   require 'plugins.conform',
   require 'plugins.copilot',
   require 'plugins.gitsigns',
-  require 'plugins.hop',
   require 'plugins.indents',
   require 'plugins.lsp',
   require 'plugins.mini',
