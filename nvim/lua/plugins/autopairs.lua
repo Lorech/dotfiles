3b46d5be194f3eb5e575c40e1d0ea65450853f80
Lorech <laurisdedumets@gmail.com>
2025-11-27 10:58:54 +0200

    fix: Readd auto-close and auto-rename via Treesitter

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,6 +1,13 @@
 -- Automatically pair symbols that should pair, e.g., parens, quotes, etc.
 return {
-  'windwp/nvim-autopairs',
-  event = 'InsertEnter',
-  opts = {},
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    opts = {},
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    opts = {},
+  },
 }
